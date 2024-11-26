5b54015a6aeb5c0a404fcc4110e6c5ea03d5fd4e
moha-salad64 <mohamedsalaad509@gmail.com>
2024-11-26 11:02:57 +0300

    deleting and updating functionality

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const container = document.querySelector('.container')
 
 
 //creating todo list items
-function createTodoListItem(){
+function createTodoListItem(task) {
 
     //creating todo list
     const todoList = document.createElement('div');
-    todoList.className ='todo-list';
+    todoList.className = 'todo-list';
     //creating tod items
     const todoItems = document.createElement('div');
+    todoItems.textContent = task;
     todoItems.className = 'todo-item';
     //creating btn update
     const btnUpdate = document.createElement('button');
@@ -25,21 +26,48 @@ function createTodoListItem(){
     container.appendChild(todoList).appendChild(btnUpdate);
     container.appendChild(todoList).appendChild(btnDelete)
 
+
+    deleteTodoItem();
+    updateTodoItem();
 }
 
 
-addTodo.addEventListener('click' , (event) =>{
+//Delete Todo List Item
+function deleteTodoItem() {
+    let current_task = document.querySelectorAll('.btnDelete');
+    current_task.forEach((tasks) => {
+        tasks.addEventListener('click', (event) => {
+            event.preventDefault();
+            tasks.parentElement.remove();
+        })
+    })
+}
+
+
+//update Tod List Item
+function updateTodoItem(){
+    let updateItem = document.querySelectorAll('.btnUpdate');
+    updateItem.forEach((item) =>{
+        item.addEventListener('click' , (event) =>{
+            event.preventDefault();
+            let todoText = item.parentElement.querySelector('.todo-item').textContent;
+            inputTodo.value = todoText;
+        })
+    })
+}
+
+addTodo.addEventListener('click', (event) => {
     event.preventDefault();
 
     let todoItem = inputTodo.value.trim();
-    if(todoItem){
-        inputTodo.style.border = '';
+    if (todoItem) {
         // console.log(`${todoItem}`)
-        createTodoListItem();
+        createTodoListItem(todoItem);
+        inputTodo.value = ""
     }
-    else{
+    else {
         inputTodo.style.border = '1px solid red'
     }
-   
+
 })
 
